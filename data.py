627c9252d5f49f22a02bd792a72402df8e43f9bd
Gregor Kikelj <gregor1234567890@gmail.com>
2024-03-25 15:03:09 +0100

    baseline ViT

--- data.py
+++ data.py
@@ -15,7 +15,6 @@ class C10kDataModule(L.LightningDataModule):
         self.batch_size = config["batch_size"]
         self.height = config["height"]
         self.width = config["width"]
-        self.lr = config["lr"]
         self.backbone = config["backbone"]
         self.augmentation_level = config["augmentation_level"]
     
@@ -27,8 +26,8 @@ class C10kDataModule(L.LightningDataModule):
         train_masks = [x for x in masks if not x.name.endswith("9.png")]
         valid_imgs = [x for x in imgs if x.name.endswith("9.png")]
         valid_masks = [x for x in masks if x.name.endswith("9.png")]
-        self.train_ds = DatasetWrapper(train_imgs, train_masks, get_transforms(self.height, self.width, self.augmentation_level), self.backbone)
-        self.val_ds = DatasetWrapper(valid_imgs, valid_masks, get_valid_transforms(self.height, self.width), self.backbone)
+        self.train_ds = DatasetWrapper(train_imgs[:5000], train_masks[:5000], None, None)
+        self.val_ds = DatasetWrapper(valid_imgs, valid_masks, None, None)
 
     def train_dataloader(self):
         return DataLoader(self.train_ds, batch_size=self.batch_size, num_workers=16, shuffle=True, drop_last=True, pin_memory=True)
@@ -36,241 +35,12 @@ class C10kDataModule(L.LightningDataModule):
     def val_dataloader(self):
         return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=8) # why does this crash with 16 workers?
 
-def pad_to_multiple(x, k=32):
-    return int(k * (np.ceil(x / k)))
-
-def get_scale_transform(height: int, width: int):
-    return A.Compose(
-        [
-            A.Resize(height=height, width=width),
-            A.PadIfNeeded(
-                pad_to_multiple(height),
-                pad_to_multiple(width),
-                border_mode=cv2.BORDER_CONSTANT,
-                value=0,
-                mask_value=-100, # cross entropy loss ignores 0
-            ),
-        ]
-    )
-
-
-def get_transforms(height: int, width: int, level: str):
-    if level == "light":
-        return A.Compose(
-            [
-                A.PadIfNeeded(
-                    pad_to_multiple(height),
-                    pad_to_multiple(width),
-                    border_mode=cv2.BORDER_CONSTANT,
-                    value=0,
-                    mask_value=-100,
-                ),
-                A.HorizontalFlip(p=0.5),
-                A.GaussNoise(p=0.2),
-                A.OneOf(
-                    [
-                        A.CLAHE(p=1.0),
-                        A.RandomBrightnessContrast(p=1.0, contrast_limit=0),
-                        A.RandomGamma(p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.Sharpen(p=1.0),
-                        A.Blur(blur_limit=3, p=1.0),
-                        A.MotionBlur(blur_limit=3, p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.RandomBrightnessContrast(p=1.0, brightness_limit=0),
-                        A.HueSaturationValue(p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.Resize(height=height, width=width, p=1.0),
-            ],
-            p=1.0,
-        )
-    elif level == "hard":
-        return A.Compose(
-            [
-                A.PadIfNeeded(
-                    pad_to_multiple(height),
-                    pad_to_multiple(width),
-                    border_mode=cv2.BORDER_CONSTANT,
-                    value=0,
-                    mask_value=-100,
-                ),
-                A.HorizontalFlip(p=0.5),
-                A.GaussNoise(p=0.2),
-                A.OneOf(
-                    [
-                        A.GridDistortion(
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                        A.ElasticTransform(
-                            alpha_affine=10,
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                        A.ShiftScaleRotate(
-                            shift_limit=0,
-                            scale_limit=0,
-                            rotate_limit=10,
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                        A.OpticalDistortion(
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.CLAHE(p=1.0),
-                        A.RandomBrightnessContrast(p=1.0, contrast_limit=0),
-                        A.RandomGamma(p=1.0),
-                        A.ISONoise(p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.Sharpen(p=1.0),
-                        A.Blur(blur_limit=3, p=1.0),
-                        A.MotionBlur(blur_limit=3, p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.RandomBrightnessContrast(p=1.0, brightness_limit=0),
-                        A.HueSaturationValue(p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.Resize(height=height, width=width, p=1.0),
-                A.CoarseDropout(p=0.3, mask_fill_value=0),
-                A.PadIfNeeded(
-                    pad_to_multiple(height),
-                    pad_to_multiple(width),
-                    border_mode=cv2.BORDER_CONSTANT,
-                    value=0,
-                    mask_value=-100,
-                ),
-            ],
-            p=1.0,
-        )
-    elif level == "hard_weather":
-        return A.Compose(
-            [
-                A.PadIfNeeded(
-                    pad_to_multiple(height),
-                    pad_to_multiple(width),
-                    border_mode=cv2.BORDER_CONSTANT,
-                    value=0,
-                    mask_value=-100,
-                ),
-                A.HorizontalFlip(p=0.5),
-                A.GaussNoise(p=0.2),
-                A.OneOf(
-                    [
-                        A.GridDistortion(
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                        A.ElasticTransform(
-                            alpha_affine=10,
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                        A.ShiftScaleRotate(
-                            shift_limit=0,
-                            scale_limit=0,
-                            rotate_limit=10,
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                        A.OpticalDistortion(
-                            border_mode=cv2.BORDER_CONSTANT,
-                            value=0,
-                            mask_value=-100,
-                            p=1.0,
-                        ),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.CLAHE(p=1.0),
-                        A.RandomBrightnessContrast(p=1.0, contrast_limit=0),
-                        A.RandomGamma(p=1.0),
-                        A.ISONoise(p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.Sharpen(p=1.0),
-                        A.Blur(blur_limit=3, p=1.0),
-                        A.MotionBlur(blur_limit=3, p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.RandomBrightnessContrast(p=1.0, brightness_limit=0),
-                        A.HueSaturationValue(p=1.0),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.RandomFog(fog_coef_upper=0.8, p=1.0),
-                        A.RandomRain(p=1.0),
-                        A.RandomSnow(p=1.0),
-                        A.RandomSunFlare(src_radius=100, p=1.0),
-                    ],
-                    p=0.4,
-                ),
-                A.Resize(height=height, width=width, p=1.0),
-                A.CoarseDropout(p=0.3, mask_fill_value=0),
-            ],
-            p=1.0,
-        )
-    else:
-        raise ValueError
-
-def get_valid_transforms(height: int, width: int):
-    return get_scale_transform(height, width)
 
 class DatasetWrapper(Dataset):
     def __init__(self, img_paths, mask_paths, transform, backbone):
         assert len(img_paths) == len(mask_paths), "Number of images and masks do not match"
         self.img_paths = img_paths
         self.mask_paths = mask_paths
-        self.transform = transform
-        self.preprocess = smp.encoders.get_preprocessing_fn(backbone)
     
     def __len__(self):
         return len(self.img_paths)
@@ -283,7 +53,4 @@ class DatasetWrapper(Dataset):
         conditions = [mask == v for v in CLASS_VALUES]
         mask_ids = np.select(conditions, list(range(5))) # (1164, 874) with 0-4 values. CLASS_VALUES determined by .ipynb
 
-        img_rgb, mask_ids = self.transform(image=img_rgb, mask=mask_ids).values()
-        img_rgb = self.preprocess(img_rgb)
-
-        return np.transpose(img_rgb.astype(np.float32), (2, 0, 1)), mask_ids.astype(np.int64)
+        return self.img_paths[idx].name, np.transpose(img_rgb.astype(np.float32), (2, 0, 1)), mask_ids.astype(np.int64)
--- model.py
+++ model.py
@@ -2,29 +2,72 @@ import torch, torch.nn as nn, torch.nn.functional as F
 import segmentation_models_pytorch as smp
 
 import lightning as L
+import pickle
+import sys
+sys.path.append("../repo")
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+with open('tds_emb_dict.pkl', 'rb') as file:
+    tds_emb_dict = pickle.load(file)
+
+with open('vds_emb_dict.pkl', 'rb') as file:
+    vds_emb_dict = pickle.load(file)
+
+sam_model = sam_model_registry["vit_h"](checkpoint="../repo/sam_vit_h_4b8939.pth")
+sam_model.train()
+sparse_prompt_embeddings, dense_prompt_embeddings = sam_model.prompt_encoder(
+        points=None,
+        boxes=None,
+        masks=None,
+    )
+dense_pos_emb = sam_model.prompt_encoder.get_dense_pe()
+sparse_prompt_embeddings = sparse_prompt_embeddings.cuda()
+dense_prompt_embeddings = dense_prompt_embeddings.cuda()
+dense_pos_emb = dense_pos_emb.cuda()
 
 def acc_fn(out, mask):
     valid_mask_count = (mask != -100).sum()
     return (torch.argmax(out, dim=1) == mask).float().sum() / valid_mask_count
-
+d = (1164-874)//2
 class Model(L.LightningModule):
     def __init__(self, config):
         super().__init__()
         self.config = config
         self.save_hyperparameters()
-        self.model = smp.DeepLabV3Plus(
-            encoder_name=config["backbone"],
-            encoder_weights="imagenet",
-            in_channels=3,
-            classes=5,
+        self.mask_decoder = sam_model.mask_decoder
+
+        self.upscale1 = nn.ConvTranspose2d(3, 3, 4, stride=2, padding=1)
+        self.upscale2 = nn.ConvTranspose2d(3, 3, 4, stride=2, padding=1)
+
+        self.refinement = nn.Sequential(
+            nn.Conv2d(6, 64, 5, padding=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 5, padding=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 5, 1),
         )
+
     
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, fnames, x, train=True):
+        dct = tds_emb_dict if train else vds_emb_dict
+        embs = torch.cat([torch.tensor(dct[fname]) for fname in fnames], dim=0).cuda()
+        low_res_masks, _ = self.mask_decoder(
+            image_embeddings=embs,
+            image_pe=dense_pos_emb,
+            sparse_prompt_embeddings=sparse_prompt_embeddings,
+            dense_prompt_embeddings=dense_prompt_embeddings,
+            multimask_output=True,
+        )
+        masks = F.relu(self.upscale1(low_res_masks))
+        masks = F.relu(self.upscale2(masks))
+        upscaled_masks = F.interpolate(masks, size=(1164, 1164), mode='bilinear', align_corners=False)
+        x = F.pad(x, (0, 0, d, d))
+        refinement_input = torch.cat([x, upscaled_masks], dim=1)
+        return self.refinement(refinement_input)[...,d:-d,:]
     
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        out = self(x)
+        fnames, x, y = batch
+        out = self(fnames, x, True)
         loss = F.cross_entropy(out, y)
         accuracy = acc_fn(out, y)
         self.log('train_loss', loss, on_step=True, on_epoch=True)
@@ -33,16 +76,8 @@ class Model(L.LightningModule):
         return {'loss': loss, 'accuracy': accuracy}
     
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-
-        out = self(x)
-
-        if self.config["TTA"]:
-            x_flipped = torch.flip(x, [3])
-            out_flipped = self(x_flipped)
-            out_flipped = torch.flip(out_flipped, [3])
-            out = (out + out_flipped) / 2
-
+        fnames, x, y = batch
+        out = self(fnames, x, False)
         loss = F.cross_entropy(out, y)
         accuracy = acc_fn(out, y)
         self.log('val_loss', loss, on_step=False, on_epoch=True)
